6619920307857e79d09130f88008698b5baab3bd
Kayo <kayo.fernandes_dev@hotmail.com>
2023-05-24 02:15:52 +0000

    udpate

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
 import flet as ft
 from page_login import Login
 
+
 class Main:
     def __init__(self) -> None:
-        ft.app(self.main,view=ft.WEB_BROWSER)
-        
-    def main(self,page:ft.Page):
+        ft.app(self.main, view=ft.WEB_BROWSER)
+
+    def main(self, page: ft.Page):
         page.title = 'Login'
         page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-        page.add(Login())
+
+        def route_change(route):
+            page.views.clear()
+            page.views.append(self.__create_view('/', [Login(page)]))
+            if page.route == '/home':
+                page.views.append(self.__create_view(
+                    '/home', [ft.Text('Home'), ft.ElevatedButton('Logout', on_click=lambda _:page.go('/'))]))
+            page.update()
+
+        def view_pop(view):
+            page.views.pop()
+            top_view = page.views[-1]
+            page.go(top_view.route)
+
+        page.on_route_change = route_change
+        page.on_view_pop = view_pop
+        page.go(page.route)
+
+    def __create_view(self, route: str, controls: list):
+        """
+        This function creates a view object with a specified route and list of controls.
+
+        :param route: The route parameter is a string that represents the URL path for the view. It is the
+        address that the user will enter in their browser to access the view
+        :type route: str
+        :param controls: The `controls` parameter is a list of `ft.Control` objects that define the user
+        interface elements for the view. These controls can include buttons, text boxes, labels, and other
+        interactive elements that allow the user to interact with the view. The `__create_view` method uses
+        these controls to
+        :type controls: list
+        :return: an instance of the `View` class from the `ft` module, which is created using the `route`
+        and `controls` arguments passed to the function.
+        """
+        return ft.View(route, controls)
+
 
 if __name__ == '__main__':
     Main()
--- page_login.py
+++ page_login.py
@@ -1,40 +1,76 @@
-from flet import TextField, Text, UserControl, TextButton, TextStyle,TextDecoration, Container,Column,icons, Alignment
+from flet import TextField, Text, UserControl, TextButton, TextStyle, TextDecoration, Container, Column, icons, Alignment
 import re
 
 
-
 class Login(UserControl):
-    
+
     def build(self):
-        self.email = TextField(hint_text='Email',on_change=self.__on_change,icon=icons.EMAIL_ROUNDED)
-        self.password = TextField(hint_text='Password',password=True,on_submit=self.__check_login,icon=icons.LOCK_ROUNDED)
-        self.login = TextButton(text='Login',on_click=self.__check_login,icon=icons.LOGIN)
-        
+        """
+        This function builds a login form with email and password fields and a login button.
+        :return: The `build` method is returning a `Column` widget that contains a `TextField` for email
+        input, a `TextField` for password input (with the `password` attribute set to `True` to hide the
+        input), and a `TextButton` for submitting the login information. The `Column` widget is set to have
+        a spacing of 10 and a width of 500.
+        """
+        self.email = TextField(
+            hint_text='Email', on_change=self.__on_change, icon=icons.EMAIL_ROUNDED)
+        self.password = TextField(hint_text='Password', password=True,
+                                  on_submit=self.__check_login, icon=icons.LOCK_ROUNDED)
+        self.login = TextButton(
+            text='Login', on_click=self.__check_login, icon=icons.LOGIN)
         return Column(
             controls=[
                 Container(self.email),
                 Container(self.password),
-                Container(self.login,alignment=Alignment(1,0))
+                Container(self.login, alignment=Alignment(1, 0))
             ],
             spacing=10,
             width=500
         )
-        
-    def __on_change(self,event):
+
+    def __on_change(self, event):
+        """
+        This function validates an email input and displays an error message if it is invalid.
+
+        :param event: The "event" parameter is an object that represents the event that triggered the
+        function. It contains information about the event, such as the type of event, the control that
+        triggered the event, and any data associated with the event. In this case, it is likely an event
+        that is triggered when the
+        """
         if not self.__validate_email(event.control.value):
             self.email.error_text = 'Invalid email'
-            self.email.error_style = TextStyle(decoration=TextDecoration.UNDERLINE)
+            self.email.error_style = TextStyle(
+                decoration=TextDecoration.UNDERLINE)
         else:
             self.email.error_text = ''
             self.email.error_style = None
         self.update()
-    
-    def __check_login(self,event):
-        if event.control.value:
+
+    def __check_login(self, event):
+        """
+        This function checks if the email value is present and prints either "Login success" or "Login
+        failed".
+
+        :param event: The event parameter is likely an object that represents the event that triggered the
+        function. It is not used in the function body, so it's hard to say exactly what type of event it is
+        without more context. However, based on the function name and the fact that it checks for a
+        non-empty email
+        """
+        if self.email.value:
             print('Login success')
+            self.page.go('/home')
         else:
             print('Login failed')
-    
+
     @staticmethod
     def __validate_email(email):
+        """
+        The function takes an email as input and is likely used to validate whether the email is in a
+        correct format. However, the function body is missing so it's unclear what specific validation
+        checks are being performed.
+
+        :param email: The "email" parameter is a string that represents an email address that needs to be
+        validated. The function likely checks if the email address is in a valid format according to certain
+        rules and returns a boolean value indicating whether the email is valid or not
+        """
         return re.match(r"[^@]+@[^@]+\.[^@]+", email)
